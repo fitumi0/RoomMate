5ca27b3694feb7b18f595580b8d43397c63f484e
fitumi0 <chervyakov2002anton@mail.ru>
2024-03-05 11:52:45 +0300

    # files moved

--- frontend/src/app/services/room/room.service.ts
+++ frontend/src/app/services/room/room.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,9 @@ export class RoomService {
   constructor(private readonly http: HttpClient) {}
   getRoom(roomId: string): Observable<Object> {
     return this.http.get(`api/get-room/${roomId}`);
+  }
+
+  getPublicRooms(): Observable<Object> {
+    return this.http.get('api/get-public-rooms');
   }
 }

41790aebc7ad84357cebe5bd9039a8990a44cff2
fitumi0 <chervyakov2002anton@mail.ru>
2024-03-05 21:37:27 +0300

    # some visual changes + template for info block

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -1,16 +1,14 @@
 import { Component } from '@angular/core';
-import { FeaturesComponent } from '../../components/features/features.component';
-import { FormatsComponent } from '../../components/formats/formats.component';
+import { InfoBlockComponent } from '../../components/home-items/info-block.component';
 import { RoomsComponent } from '../../components/rooms/rooms.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
-
+import { Item } from '../../types/Item';
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    FeaturesComponent,
-    FormatsComponent,
+    InfoBlockComponent,
     RoomsComponent,
     HeaderComponent,
     FooterComponent,
@@ -18,4 +16,44 @@ import { FooterComponent } from '../../components/footer/footer.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  features: Item[] = [
+    {
+      icon: 'sync',
+      title: 'SYNCHRONIZED PLAY',
+      description: 'Watch video with your friends in sync.',
+    },
+    {
+      icon: 'chat',
+      title: 'TEXT CHAT',
+      description: 'Chat with your friends while watching videos.',
+    },
+    {
+      icon: 'videocam',
+      title: 'VIDEO CHAT',
+      description: 'See your friends in real time.',
+    },
+    {
+      icon: 'mic',
+      title: 'VOICE CHAT',
+      description: 'Talk with your friends while watching videos.',
+    },
+  ];
+  formats: Item[] = [
+    {
+      icon: 'folder',
+      title: 'LOCAL FILES',
+      description: 'Share files directly from your device.',
+    },
+    {
+      icon: 'desktop_windows',
+      title: 'SCREENSHARE',
+      description: 'Share your desktop screen or a browser tab.',
+    },
+    {
+      icon: 'link',
+      title: 'ANY VIDEO URLS',
+      description: 'Share video URLs directly from your device.',
+    },
+  ];
+}
---
+++ frontend/src/app/types/Item.ts
@@ -0,0 +1,5 @@
+export type Item = {
+  icon: string;
+  title: string;
+  description: string;
+};

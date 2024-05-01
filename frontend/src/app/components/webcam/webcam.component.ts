6170eaa283622fe51c6327cd4f34619c2fcf70ca
Solovey Roman <gasoss221510@gmail.com>
2024-05-01 15:03:42 +0300

    add webcam logic

--- frontend/src/app/components/webcam/webcam.component.ts
+++ frontend/src/app/components/webcam/webcam.component.ts
@@ -1,11 +1,16 @@
 import {
+  AfterViewInit,
   CUSTOM_ELEMENTS_SCHEMA,
+  ChangeDetectorRef,
   Component,
   ElementRef,
+  Inject,
   Input,
   OnChanges,
+  PLATFORM_ID,
   SimpleChanges,
   ViewChild,
+  isDevMode,
 } from '@angular/core';
 import 'vidstack/player/styles/default/theme.css';
 import 'vidstack/player/styles/default/layouts/video.css';
@@ -13,23 +18,32 @@ import 'vidstack/player';
 import 'vidstack/player/layouts/default';
 import 'vidstack/player/ui';
 import { MediaPlayerElement, MediaProviderElement } from 'vidstack/elements';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 @Component({
   selector: 'app-webcam',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './webcam.component.html',
   styleUrl: './webcam.component.scss',
 })
-export class WebcamComponent implements OnChanges {
-  @Input() src!: MediaStream;
+export class WebcamComponent implements  AfterViewInit {
+  @Input() stream: MediaStream | null = null;
   @Input() id: string | number = '';
-  @ViewChild('webcam') webcam!: ElementRef<MediaPlayerElement>;
+  @ViewChild('webcamElement') webcamElement: ElementRef | undefined;
 
-  constructor() {
-    (window as any)['webcam'] = this.webcam;
+  constructor(
+    private readonly cdr: ChangeDetectorRef,
+    @Inject(PLATFORM_ID) private platformId: any,
+  ) {
+    
   }
-  ngOnChanges(changes: SimpleChanges): void {
-    console.log(this.webcam);
+  ngAfterViewInit(): void {
+    if (this.webcamElement && this.stream) {
+      this.webcamElement.nativeElement.src = this.stream;
+      this.webcamElement.nativeElement.autoPlay = true;
+      this.cdr.detectChanges();
+    }
   }
+
 }

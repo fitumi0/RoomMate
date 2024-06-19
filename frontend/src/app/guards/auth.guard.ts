0762ad9c3021254c56d4a86094867c6168632c5b
Solovey Roman <gasoss221510@gmail.com>
2024-06-19 12:15:56 +0300

    fix refreshe profile page

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,6 +1,10 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { inject } from '@angular/core';
 import { AuthService } from '../services/auth/auth.service';
+import { Observable } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import { PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 /**
  * Function that checks if the user is authenticated.
@@ -9,14 +13,34 @@ import { AuthService } from '../services/auth/auth.service';
  * @returns CanActivateFn
  */
 export function authGuard(): CanActivateFn {
-  return () => {
-    const auth: AuthService = inject(AuthService);
-    const router: Router = inject(Router);
+    return (): Observable<boolean> => {
+        console.log('from authGuard');
+        const auth: AuthService = inject(AuthService);
+        const router: Router = inject(Router);
 
-    if (!auth.isAuthSig()) {
-      router.navigate(['/signin']);
-    }
-
-    return auth.isAuthSig();
-  };
+        if (isPlatformBrowser(inject(PLATFORM_ID))) {
+            return auth.getUser().pipe(
+                map(user => {
+                    if (user) {
+                        console.log('user', user);
+                        return true;
+                    } else {
+                        console.log('no user');
+                        router.navigate(['/signin']);
+                        return false;
+                    }
+                }),
+                catchError(() => {
+                    console.log('error fetching user');
+                    router.navigate(['/signin']);
+                    return [false];
+                })
+            );
+        } else {
+            return new Observable<boolean>(observer => {
+                observer.next(false);
+                observer.complete();
+            });
+        }
+    };
 }

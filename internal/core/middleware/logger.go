a4cf59c111059400b8c45d18522e72dff47d0e73
fitumi0 <chervyakov2002anton@mail.ru>
2025-04-01 00:06:42 +0300

    separated factory and mw from api

--- internal/core/middleware/logger.go
+++ internal/core/middleware/logger.go
@@ -7,6 +7,7 @@ import (
 
 // LoggingMiddleware
 func LoggingMiddleware(next http.Handler) http.Handler {
+	// TODO: change to normal logger
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("[%s]: %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)

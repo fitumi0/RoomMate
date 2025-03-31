09e6f9a6638487d0f86b8c0068c2a693c470d406
fitumi0 <chervyakov2002anton@mail.ru>
2025-03-31 21:24:10 +0300

    implemented RequiresAuth flag for request auth access

--- internal/api/routes/routes.go
+++ internal/api/routes/routes.go
@@ -2,12 +2,15 @@ package routes
 
 import (
 	"net/http"
-	"roommate/internal/api/handlers"
+	httphandlers "roommate/internal/api/handlers"
 	"roommate/internal/api/payload"
 	"roommate/internal/router"
 )
 
-type HandlerMap map[string]http.HandlerFunc
+type HandlerMap map[string]struct {
+	Handler      http.HandlerFunc
+	RequiresAuth bool
+}
 
 // Route represents a single route configuration
 type Route struct {
@@ -16,13 +19,26 @@ type Route struct {
 }
 
 // RegisterRoutes registers all application routes
-func RegisterRoutes(router router.Router, handlers *handlers.Handlers) {
+func RegisterRoutes(router router.Router, handlers *httphandlers.Handlers) {
 	routes := []Route{
 		{
 			Path: "/api/room",
 			Handlers: HandlerMap{
-				payload.METHOD_POST: handlers.Room.CreateRoom,
-				payload.METHOD_GET:  handlers.Room.GetRoom,
+				payload.METHOD_POST: {
+					Handler: handlers.Room.CreateRoom,
+				},
+				payload.METHOD_GET: {
+					Handler: handlers.Room.GetRoom,
+				},
+			},
+		},
+		{
+			Path: "/api/public-rooms",
+			Handlers: HandlerMap{
+				payload.METHOD_GET: {
+					Handler:      handlers.Room.GetPublicRooms,
+					RequiresAuth: true,
+				},
 			},
 		},
 		// Add more routes here as needed
@@ -31,8 +47,13 @@ func RegisterRoutes(router router.Router, handlers *handlers.Handlers) {
 	// Register all routes
 	for _, route := range routes {
 		router.HandleFunc(route.Path, func(w http.ResponseWriter, r *http.Request) {
-			if handler, exists := route.Handlers[r.Method]; exists {
-				handler(w, r)
+			if handlerEntry, exists := route.Handlers[r.Method]; exists {
+				if handlerEntry.RequiresAuth {
+					// TODO: implement auth
+					httphandlers.NotFoundHandler(w, r)
+					return
+				}
+				handlerEntry.Handler(w, r)
 			} else {
 				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 			}

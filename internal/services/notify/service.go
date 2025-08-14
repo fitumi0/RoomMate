95702b5de69b0c00c59d9cdef100292ea7357ade
fitumi0 <chervyakov2002anton@mail.ru>
2025-08-14 23:34:43 +0300

    notify stub

---
+++ internal/services/notify/service.go
@@ -0,0 +1,20 @@
+package notify
+
+// import (
+// 	"roommate/internal/repository/processing"
+// 	"roommate/internal/repository/storage"
+// )
+
+// type ProcessingService struct {
+// 	processingRepository *processing.ProcessingRepository
+// 	storageRepository    *storage.StorageRepository
+// }
+
+// func NewProcessingService(processingRepository *processing.ProcessingRepository, storageRepository *storage.StorageRepository) *ProcessingService {
+// 	return &ProcessingService{processingRepository: processingRepository, storageRepository: storageRepository}
+// }
+type NotifyService struct{}
+
+func NewNotifyService() *NotifyService {
+	return &NotifyService{}
+}
